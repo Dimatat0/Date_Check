c61e17a74f5d25f5c3c9f662afd41f7bb564f223
Dmitry Tatarskikh <tatarskih51@gmail.com>
2023-06-13 10:33:27 +0300

    Мердж деф и маин, после мерджа теста 2

--- Abb.cpp
+++ Abb.cpp
@@ -8,9 +8,14 @@ using namespace std;
 
 bool dateisTrueCheck(string i)
 {
+
 	const regex r(R"(^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).[0-9]{4}$)");
 	smatch m;
 	return regex_match(i, m, r);
+
+}
+bool dateisTrueChecking(string i)
+{
 	const regex r(R"(^\d{2}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])$)");
 	smatch m;
 	return regex_match(i, m, r);
@@ -18,11 +23,17 @@ bool dateisTrueCheck(string i)
 
 int main()
 {
-	string a = "20/02/9";
-	string b = "20/02/39";
-	string c = "20/02/29";
+
+	string a = "1.02.2004";
+	string b = "31.02.2004";
 	cout << dateisTrueCheck(a);
 	cout << dateisTrueCheck(b);
-	cout << dateisTrueCheck(c);
+
+	string d = "20/02/9";
+	string e = "20/02/39";
+	string f = "20/02/29";
+	cout << dateisTrueChecking(d);
+	cout << dateisTrueChecking(e);
+	cout << dateisTrueChecking(f);
 
 }
