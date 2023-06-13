6a7949ce143772cbd70f0050ea8fc1bd9e1f051c
Dmitry Tatarskikh <tatarskih51@gmail.com>
2023-06-13 09:56:36 +0300

    Испрален 39 инцидент

--- Abb.cpp
+++ Abb.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool dateisTrueCheck(string i)
 {
-	const regex r(R"(^[0-3][0-9].[0-3][0-9].(?:[0-9][0-9])?[0-9][0-9]$)");
+	const regex r(R"(^(3[01]|[12][0-9]|0[1-9]).(1[0-2]|0[1-9]).[0-9]{4}$)");
 	smatch m;
 	return regex_match(i, m, r);
 }
@@ -16,6 +16,8 @@ bool dateisTrueCheck(string i)
 int main()
 {
 	string a = "1.02.2004";
+	string b = "31.02.2004";
 	cout << dateisTrueCheck(a);
+	cout << dateisTrueCheck(b);
 
 }
